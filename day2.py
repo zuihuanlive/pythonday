1c38b9828544f878a641f2bec96f3018af803606
zuilive <zuihuanxiang@126.com>
2015-11-19 14:57:40 +0800

    添加类练习

--- day2.py
+++ day2.py
@@ -1,8 +1,20 @@
 # coding:utf-8
 
-import os
+# import os
+import datetime
+import  time
 
-for root, dirs, files in os.walk('/home/zuilive/PycharmProjects'):
-    for f in files:
-        print(f)
+# for root, dirs, files in os.walk('/home/zuilive/PycharmProjects'):
+#     for f in files:
+#         print(f)
+
+exec_time = '02:30'
+exec_time.split(':')
+hour = exec_time[0]
+minute = exec_time[1]
+# today = datetime.date.today()
+# now = datetime.date.ctime()
+# now = time.ctime
+today = int(time.strftime("%M", time.localtime(time.time())))
+print(today+1)
 
--- python/class_1.py
+++ python/class_1.py
@@ -1,18 +1,45 @@
-#coding:utf-8
+# coding:utf-8
 class Monk:
     def __init__(self, name, age):
         self.name = name
         self.age = age
     
     def speech(self):
-        print '你好'
+        print('你好')
 
     def net(self):
-        print '世界'
-#实例化
+        print('世界')
+# 实例化
 ye = Monk('zuilive',23)
 
-#调用方法
+# 调用方法
 ye.speech()
 
 ye.net()
+
+# 指定超类
+
+
+class Fiter:
+	def init(self):
+		self.blocked = []
+	def filter(self, sequence):
+		return [x for x in sequence if x not in self.blocked]
+
+
+class SPAMFilter(Fiter): # SPAMFilter是Filter的子类
+	def init(self): # 重写Filter超类中的init方法
+		self.blocked = ['SPAM']
+
+s = SPAMFilter()
+s.init()
+print(s.filter(['SPAM', 'Spam', 'sPam', 'SPAM', 'egg', 'bacon']))
+
+
+# 构造方法
+class FooBar:
+    def __init__(self, value=42):
+        self.somevar = value
+
+f = FooBar()
+print(f.somevar)
